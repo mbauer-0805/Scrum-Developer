66560e45a5bf717182f8bafb181977b993957bd9
Hrbatovic <e.hrbatovic@accenture.com>
2018-12-17 14:24:42 +0100

    adding product owner image

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import devteamgrafik from './devTeam.png';
+import productownergrafik from './productOwner.png';
+import berlingrafik from './berlin.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Berliner Schmeh</h1>
-          <h3>Introducing SCRUM Product Owner and Development Team.</h3>
+          <img src={berlingrafik} className="App-logo" alt="logo" />
+          <h1 className="App-title">Team Berliner Schmeh</h1>
         </header>
-        <p className="App-intro">
-          <img src={devteamgrafik} alt="devteam bild" />
+        
+        <p className="DevTeam-intro">
+          <h2>Development Team</h2>
+          <img src={devteamgrafik} width="830" alt="devteam bild" />
         </p>
+        
+        <p className="ProductOwner-intro">
+          <h2>Product Owner</h2>
+          <img src={productownergrafik} alt="productowner bild" />
+        </p>
+
       </div>
     );
   }
