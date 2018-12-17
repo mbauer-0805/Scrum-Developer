77d85ce3ab9eddc939d2cf95c2112465204ff733
Hrbatovic <e.hrbatovic@accenture.com>
2018-12-17 18:10:38 +0100

    adding sprint

--- src/App.js
+++ src/App.js
@@ -81,6 +81,30 @@ During the Sprint Retrospective, the team discusses:</p>
           
         </ul>
         </div>
+
+
+        <p className="berlinercontent">
+          <h2>The Sprint</h2>
+        </p>
+
+<div className="contentleft">
+      <p>
+      As described in the Scrum Guide, a Sprint, a time-box of one month or less during which a “Done”, useable, and Scrum Sprint is part of the Empirical Processpotentially releasable product Increment is created. Sprints have consistent durations throughout a development effort. A new Sprint starts immediately after the conclusion of the previous Sprint.  
+      </p>
+
+      <p>
+        <ul>
+          <li>No changes are made that would endanger the Sprint Goal</li>
+          <li>Quality goals do not decrease</li>
+          <li>Scope may be clarified and re-negotiated between the Product Owner and Development Team as more is learned.</li>
+        </ul>
+      </p>
+
+      <p>
+      Each Sprint may be considered a project with no more than a one-month horizon. Like projects, Sprints are used to accomplish something. Each Sprint has a goal of what is to be built, a design and flexible plan that will guide building it, the work, and the resultant product increment.
+      </p>
+</div>
+
       </div>
 
 
