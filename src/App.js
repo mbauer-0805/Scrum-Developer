29b586788565d6afde3e8598736cbfbebcd10160
JakobGeringer <jakob.geringer@accenture.com>
2018-12-17 14:15:43 +0100

    change Logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './huoLong.png';
 import './App.css';
+import CustomComponent from "./CustomComponent";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <CustomComponent title={"Digga"} description={"Dominik Stinkt"}/>
+            </div>
+        );
+    }
 }
 
 export default App;
