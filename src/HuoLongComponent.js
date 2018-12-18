9af8f3f08982c1e622cdabd1d12c7c12539e16aa
KingPao <jakobgeringer@gmail.com>
2018-12-18 12:02:21 +0100

    add continious integration and sprint planning

--- src/HuoLongComponent.js
+++ src/HuoLongComponent.js
@@ -9,6 +9,9 @@ import increment from "./huoLongRes/increment.jpg";
 import epic from "./huoLongRes/epic.png";
 import idealDay from "./huoLongRes/idealDay.jpg";
 import storyCard from "./huoLongRes/Story_Card.png";
+import contIntegrationBrands from "./huoLongRes/ContiniousIntegration.png";
+import contIntegration from "./huoLongRes/ContiniousIntegration2.png";
+import sprintPlanning from "./huoLongRes/sprint_planning.jpg";
 
 class HuoLongComponent extends Component {
 
@@ -198,7 +201,36 @@ class HuoLongComponent extends Component {
                     </ul>
                     <img src={idealDay} className="CustomComponent-Img" alt="logo"/>
 
+                    <h2>Continious Integration</h2>
+                    <img src={contIntegration} className="CustomComponent-Img" alt="logo"/>
+                    <img src={contIntegrationBrands} className="CustomComponent-Img" alt="logo"/>
+
+                    <h2>Sprint Planning</h2>
+                    <img src={sprintPlanning} className="CustomComponent-Img" alt="logo"/>
+
+                    <p>Who?</p>
+                    <ul>
+                        <li>Scrum Master: make sure the event takes place</li>
+                        <li>Product Owner: clarifies the details of the candidate product backlog items</li>
+                        <li>The team members: discuss the detail, ask questions, choose items for sprint backlog</li>
+                    </ul>
+                    <p>When&nbsp;</p>
+                    <ul>
+                        <li>on 1st day of the Sprint</li>
+                        <li>8 hours time-boxed for one month Sprint</li>
+                    </ul>
+                    <p>Answer 2 main questions:</p>
+                    <ul>
+                        <li>What can be delivered in the Increment of the upcoming&nbsp;sprint?</li>
+                        <li>How to achieve it?</li>
+                    </ul>
+                    <p>Results</p>
+                    <ul>
+                        <li><strong>A Sprint goal</strong>: one-or two-sentence, describes what the team plans to do achieve during the Sprint.</li>
+                        <li><strong>A Sprint backlog</strong></li>
+                    </ul>
                 </div>
+
             </div>
         );
     }
