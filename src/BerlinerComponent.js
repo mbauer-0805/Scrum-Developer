f59209892c0d796304fd4f183f772fcea252c069
Mk <mehtab_kiani@yahoo.com>
2018-12-18 16:02:34 +0100

    "pizza pic"

--- src/BerlinerComponent.js
+++ src/BerlinerComponent.js
@@ -10,7 +10,8 @@ import retrospectivegrafik from "./berlinerRes/SprintRetro.png";
 import csgrafik from "./berlinerRes/cs.png";
 import storypoints from "./berlinerRes/Estimating.jpg";
 import Transboard from "./berlinerRes/Transboard.jpg";
-import Transparency from "./berlinerRes/Transparency.jpg";
+import Transparency from "./berlinerRes/Transparency.png";
+import Pizza from "./berlinerRes/pizzad.png";
 import agileBranchingGrafik from "./berlinerRes/agileBranching.jpg";
 import burndown from './berlinerRes/burdown.png';
 
@@ -226,8 +227,12 @@ class BerlinerComponent extends Component {
 
                 <p className="berlinercontent">
                     <h2>Fit For Purpose</h2>
-                    <li> Fitness criteria are metrics that measure things customers value when selecting a service again & again</li>
+                </p>
 
+                <div className="contentleft">
+                <p>Fitness criteria are metrics that measure things customers value when selecting a service again & again</p>
+                    
+                <img src={Pizza} width="800px" alt="pizza bild"></img>
                     <li>Quality</li>
                     <li>Predictability</li>
                     <li>Safety (or conformance to regulatory requirements)</li>
@@ -241,7 +246,7 @@ class BerlinerComponent extends Component {
                     <h3 >Lesson:</h3>
                     <li> We need to offer a selection of different recipes which are tasty and popular. However, we must also deliver with speed & predictability</li>
 
-                    Different segments of customers want different things. If you are trying to be the best, you need to consider which segments you are targeting. It’s unlikely you are going to please everybody all of the time. In other words, you are going to need to focus on a SOME of the possible market. Considering this helps you find your niche markets. You then need to ask yourself questions about their values, such as:
+                    <p>Different segments of customers want different things. If you are trying to be the best, you need to consider which segments you are targeting. It’s unlikely you are going to please everybody all of the time. In other words, you are going to need to focus on a SOME of the possible market. Considering this helps you find your niche markets. You then need to ask yourself questions about their values, such as:</p>
 <li> What will make them choose my product? What will turn them off?</li>
                     <li>What will make them become a repeat customer? What will scare them away forever? </li>
                     <li> What will cause them to recommend me to others? What will make them tell everyone to stay away?</li>
@@ -250,7 +255,7 @@ class BerlinerComponent extends Component {
                     It relates strongly to the first agile Principle
 <li>Our highest priority is to satisfy the customer through early and continuous delivery.</li>
 
-                </p>
+                </div>
 
                 <p className="berlinercontent">
                     <h2>Communicate work: plan and track</h2>
