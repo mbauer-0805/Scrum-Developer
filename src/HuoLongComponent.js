0186a7aeaf3d292438983f0400919bb2dd57cbaf
KingPao <jakobgeringer@gmail.com>
2018-12-19 10:12:11 +0100

    add unit test practices content

--- src/HuoLongComponent.js
+++ src/HuoLongComponent.js
@@ -26,6 +26,8 @@ import bugreport from "./huoLongRes/bug-report.jpg";
 import tdd from "./huoLongRes/TDD.jpg";
 import unittest from "./huoLongRes/UnitTest.png";
 import meme from "./huoLongRes/meme.png";
+import jUnit from "./huoLongRes/jUnitTest.PNG";
+
 
 class HuoLongComponent extends Component {
 
@@ -425,6 +427,27 @@ class HuoLongComponent extends Component {
                         </li>
                     </ul>
                     <img src={meme} className="CustomComponent-Img" alt="logo"/><br/>
+
+                </div>
+                <div className="contentleft">
+                    <h2 className="berlinercontent">Best practices for unit test</h2>
+
+                    <ul>
+                        <li>Naming your tests: explicitly express the intent</li>
+                        <li>method name</li>
+                        <li>scenario</li>
+                        <li>expected behavior</li>
+                        <li>Arrange your tests</li>
+                        <li>Arrange: create, setting up</li>
+                        <li>Act&nbsp;</li>
+                        <li>Assert: expected behavior?</li>
+                        <li>Input used in test should be simplest possible</li>
+                        <li>Avoid logic in tests: no if, for, switch...</li>
+                        <li>Helper methods to setup and teardown</li>
+                        <li>Avoid multiple asserts: create a separate test for each assert</li>
+                        <li>Validate private methods by unit testing public methods</li>
+                    </ul>
+                    <img src={jUnit} className="CustomComponent-Img" alt="logo"/><br/>
 
                 </div>
             </div>
